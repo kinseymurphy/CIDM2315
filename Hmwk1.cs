de4ee52c8a3db698ac6879fd246ffa762d910306
kinseymurphy <kmurphy1@buffs.wtamu.edu>
2025-01-22 10:58:18 -0500

    Add files via upload

---
+++ Hmwk1.cs
@@ -0,0 +1,17 @@
+﻿/*Author: Kinsey Murphy
+    Homework1
+*/
+namespace Homework1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //code for Homework1 q1 (5pts)
+        float x = 2.5f;
+        float y = 3.3f;
+        float z = 4*x*x + 3*y;
+        Console.WriteLine("X = 2.5, Y = 3.3");
+        Console.WriteLine("The value of Z is: " + z);
+    }
+}
